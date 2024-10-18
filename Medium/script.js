5f56b3c5241aea013f31a4f9e7e66f1fb1dfc37d
Nathan Price <gravityfargo@gmail.com>
2024-10-18 00:02:03 -0400

    added some logging, removed cache doc feature

--- Medium/script.js
+++ Medium/script.js
@@ -1,23 +1,24 @@
 // ==UserScript==
 // @name         Medium Auto Redirect
-// @version      0.0.4
+// @version      0.0.5
 // @description  Redirects based on predefined conditions
 // @author       Nathan Price
 // @license      GPL-3.0
 // @match        *://*/*
-// @run-at       document-start
+// @run-at       document-body
 // @grant        GM_getResourceText
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=medium.com
 // @homepageURL  https://github.com/gravityfargo/TamperMonkeyScripts
 // @downloadURL  https://raw.githubusercontent.com/gravityfargo/TamperMonkeyScripts/refs/heads/main/Medium/script.js
 // @updateURL    https://raw.githubusercontent.com/gravityfargo/TamperMonkeyScripts/refs/heads/main/Medium/script.js
 // @supportURL   https://github.com/gravityfargo/TamperMonkeyScripts/issues
-// @resource     mediumDomainList https://raw.githubusercontent.com/gravityfargo/TamperMonkeyScripts/refs/heads/main/Medium/domainlist.json
 // ==/UserScript==
 
 const windowLocationHost = unsafeWindow.location.host
 const windowLocationHref = unsafeWindow.location.href
-const mediumDomainList = JSON.parse(GM_getResourceText('mediumDomainList'))
+
+// @resource     mediumDomainList https://raw.githubusercontent.com/gravityfargo/TamperMonkeyScripts/refs/heads/main/Medium/domainlist.json
+// const mediumDomainList = JSON.parse(GM_getResourceText('mediumDomainList'))
 
 function checkIfSecretlyMedium () {
   const metaTag = document.querySelector('meta[property="al:ios:app_name"]')
@@ -30,6 +31,7 @@ function checkIfSecretlyMedium () {
 function checkIfArticle () {
   const metaTag = document.querySelector('meta[property="og:type"]')
   if (metaTag === null) {
+    console.log('No og:type meta tag')
     return false
   }
   return metaTag.content.includes('article')
@@ -51,17 +53,18 @@ function checkDomain () {
   // If the Android meta URL is a medium:// URL
   if (checkIfSecretlyMedium()) {
     if (checkIfArticle()) {
+      console.log('This is a secretly medium.com article')
       pushFreediumURL()
     }
-    return
   }
 
   // Otherwise, check if the current domain is in the list
-  if (mediumDomainList.includes(windowLocationHost)) {
-    if (checkIfArticle()) {
-      pushFreediumURL()
-    }
-  }
+  // if (mediumDomainList.includes(windowLocationHost)) {
+  //   if (checkIfArticle()) {
+  //     console.log('This is a medium.com article')
+  //     pushFreediumURL()
+  //   }
+  // }
 }
 
 checkDomain()
