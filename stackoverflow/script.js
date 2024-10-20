2cbde82d52aaf4fe34fc576b87b6bc17a4be6b8a
Nathan Price <gravityfargo@gmail.com>
2024-10-20 00:41:46 -0400

    new delete system, added serverfault

--- stackoverflow/script.js
+++ stackoverflow/script.js
@@ -6,6 +6,7 @@
 // @author       Nathan Price
 // @license      GPL-3.0
 // @match        *://stackoverflow.com/*
+// @match        *://serverfault.com/*
 // @run-at       document-idle
 // @icon         https://cdn.sstatic.net/Sites/stackoverflow/Img/apple-touch-icon.png
 // @homepageURL  https://github.com/gravityfargo/TamperMonkeyScripts
@@ -14,34 +15,37 @@
 // @supportURL   https://github.com/gravityfargo/TamperMonkeyScripts/issues
 // ==/UserScript==
 
-document.body.querySelector('header.s-topbar.ps-fixed.t0.l0.js-top-bar div.s-topbar--container ol.s-navigation').remove()
-const posts = document.body.querySelector('#mainbar').querySelectorAll('.post-layout')
-
-posts.forEach((post) => {
-  const postVoteContainer = post.querySelector('.js-voting-container')
-  Array.from(postVoteContainer.children).forEach((child) => {
-    if (!child.classList.contains('js-vote-count') && !child.classList.contains('js-accepted-answer-indicator')) {
-      child.remove()
+function clearList (objs, needParent = false) {
+  objs.forEach((obj) => {
+    if (obj) {
+      console.log(obj)
+      if (needParent) {
+        if (obj.parentElement) {
+          obj.parentElement.remove()
+          return
+        }
+      }
+      obj.remove()
     }
   })
+}
 
-  post.querySelector('.js-post-menu').parentElement.remove()
-  post.querySelector('.comments-link').parentElement.remove()
-
+function cleanComments (post) {
+  const deleteObjs = []
   const userInfos = post.querySelectorAll('.user-info')
   userInfos.forEach((user) => {
-    user.querySelector('.user-details').remove()
-    user.querySelector('.user-gravatar32').remove()
+    deleteObjs.push(user.querySelector('.user-details'))
+    deleteObjs.push(user.querySelector('.user-gravatar32'))
 
     const comments = post.querySelectorAll('.comment-body')
     comments.forEach((comment) => {
       comment.classList.add('d-flex', 'fd-column')
       if (comment.children.length === 4) {
-        comment.children[3].remove()
+        deleteObjs.push(comment.children[3])
       }
       if (comment.children.length === 3) {
         comment.children[2].classList.add('d-flex', 'jc-end')
-        comment.children[1].remove()
+        deleteObjs.push(comment.children[1])
       }
     })
   })
@@ -49,33 +53,84 @@ posts.forEach((post) => {
   const footer = userInfos[0].parentElement.parentElement
   footer.removeAttribute('class')
   footer.classList.add('d-flex', 'jc-space-between')
+  clearList(deleteObjs)
+}
 
-  if (post.id === 'question') {
-    const hr = document.createElement('hr')
-    post.insertAdjacentElement('afterend', hr)
-  }
-})
-
-const sidebar = document.querySelector('#sidebar')
-const unwantedClasses = ['hot-network-questions', 'js-feed-link', 'js-zone-container', 's-sidebarwidget', 'tex2jax_ignore', 's-sidebarwidget', 's-modal']
-const unwantedIds = ['hot-network-questions', 'hireme']
-
-Array.from(sidebar.children).forEach((child) => {
-  if (!child.hasAttribute('class')) {
-    child.remove()
-  }
-
-  if (child.hasAttribute('id')) {
-    if (unwantedIds.includes(child.id)) {
+function removeVotes (post) {
+  const postVoteContainer = post.querySelector('.js-voting-container')
+  Array.from(postVoteContainer.children).forEach((child) => {
+    if (!child.classList.contains('js-vote-count') && !child.classList.contains('js-accepted-answer-indicator')) {
       child.remove()
     }
-  }
+  })
+}
 
-  const firstClass = child.classList[0]
-  if (unwantedClasses.includes(firstClass)) {
-    child.remove()
-  }
+function init () {
+  const deleteObjs = []
+  const deleteParentObjs = []
+  const mainContent = document.getElementById('mainbar')
+
+  deleteObjs.push(document.getElementById('post-form')) // Your Answer form at the bottom
+  deleteObjs.push(document.body.querySelector('header.s-topbar.ps-fixed.t0.l0.js-top-bar div.s-topbar--container ol.s-navigation')) // About / Products / OverflowAI
+  deleteObjs.concat(mainContent.querySelectorAll('.bottom-notice')) // Not the answer you're looking for....
+  deleteObjs.push(mainContent.querySelector('aside.post-notice')) // hot topic
+
+  mainContent.querySelectorAll('.post-layout').forEach((post) => {
+    deleteParentObjs.push(post.querySelector('.js-post-menu')) // Share / Improve this question / Follow
+    deleteParentObjs.push(post.querySelector('.comments-link')) // add a comment
+
+    removeVotes(post)
+
+    // add a pretty little hr after the question
+    if (post.id === 'question') {
+      const hr = document.createElement('hr')
+      post.insertAdjacentElement('afterend', hr)
+    }
+
+    cleanComments(post)
+
+    clearList(deleteObjs)
+    clearList(deleteParentObjs, true)
+  })
+
+  const sidebar = document.querySelector('#sidebar')
+  const unwantedClasses = ['hot-network-questions', 'js-feed-link', 'js-zone-container', 's-sidebarwidget', 'tex2jax_ignore', 's-sidebarwidget', 's-modal']
+  const unwantedIds = ['hot-network-questions', 'hireme']
+
+  Array.from(sidebar.children).forEach((child) => {
+    if (!child.hasAttribute('class')) {
+      child.remove()
+    }
+
+    if (child.hasAttribute('id')) {
+      if (unwantedIds.includes(child.id)) {
+        child.remove()
+      }
+    }
+
+    if (unwantedClasses.includes(child.classList[0])) {
+      child.remove()
+    }
+  })
+
+  clearList([document.getElementById('left-sidebar')])
+}
+
+function observe (node, callback, options) {
+  const observer = new MutationObserver((mutations, ob) => {
+    const result = callback(mutations, ob)
+    if (result) disconnect()
+  })
+  observer.observe(node, Object.assign({
+    childList: true,
+    subtree: true
+  }, options))
+
+  const disconnect = () => observer.disconnect()
+
+  return disconnect
+}
+
+observe(document.body, () => {
+  init()
 })
-
-// Remove left sidebar
-document.getElementById('left-sidebar').remove()
