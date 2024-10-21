d9f72399309d0464c83841ba45bcca5937721260
Nathan Price <gravityfargo@gmail.com>
2024-10-21 19:04:29 -0400

    update

--- GLOBAL/script.js
+++ GLOBAL/script.js
@@ -1,34 +1,51 @@
 // ==UserScript==
 // @name         Gravityfargo's Global Script
 // @namespace    Violentmonkey Scripts
-// @version      0.0.1
+// @version      0.0.2
 // @description  Useful Scripts for general browsing
 // @author       Nathan Price
 // @license      GPL-3.0
-// @include      http://*
-// @include      https://*
 // @run-at       document-end
 // @grant        GM_getResourceText
+// @grant        GM_notification
 // @resource     domainDataUpstream https://raw.githubusercontent.com/gravityfargo/TamperMonkeyScripts/refs/heads/main/GLOBAL/domainData.json
 // @icon         https://globelifefield.com/wp-content/themes/globe-life-field/img/icons/apple-touch-icon.png
 // @homepageURL  https://github.com/gravityfargo/TamperMonkeyScripts
 // @downloadURL  https://raw.githubusercontent.com/gravityfargo/TamperMonkeyScripts/refs/heads/main/GLOBAL/script.js
 // @updateURL    https://raw.githubusercontent.com/gravityfargo/TamperMonkeyScripts/refs/heads/main/GLOBAL/script.js
 // @supportURL   https://github.com/gravityfargo/TamperMonkeyScripts/issues
+// @include      http://*
+// @include      https://*
 // ==/UserScript==
+//
+
+// id query: 'id-of-element|parent|#of-parents-above-to-remove'
 
 const windowLocationHost = unsafeWindow.location.host
 const domainDataUpstream = JSON.parse(GM_getResourceText('domainDataUpstream'))
 const domainDataLocal = {
+  'dev.to': {
+    queryOne: ['#page-content-inner div.crayons-layout.crayons-layout--3-cols.crayons-layout--article aside.crayons-layout__sidebar-right div.crayons-article-sticky.grid.gap-4.break-word.js-billboard-container'],
+    queryAll: [],
+    ids: ['runtime-banner-container|sibling|up|1']
+  }
 }
 
 const domainData = { ...domainDataUpstream, ...domainDataLocal }
 const matchedDomainKey = Object.keys(domainData).find((domain) => windowLocationHost.includes(domain))
 
+const wordpressData = {
+  queryOne: [],
+  queryAll: ['.boxzilla-container', '.boxzilla-overlay', 'd_unit', 'd-unit'],
+  ids: [
+  ]
+}
+
 function removeById (domainInfo) {
   domainInfo.ids.forEach((item) => {
     const items = item.split('|')
     const element = document.getElementById(items[0])
+    console.log('Removing:', items[0])
 
     if (element) {
       if (item.includes('|parent')) {
@@ -71,6 +88,7 @@ function removeById (domainInfo) {
 
 function removeByQueryAll (domainInfo) {
   domainInfo.queryAll.forEach((item) => {
+    console.log('Removing:', item)
     const els = document.body.querySelectorAll(item)
     els.forEach((el) => {
       if (el) {
@@ -82,6 +100,7 @@ function removeByQueryAll (domainInfo) {
 
 function removeByQuery (domainInfo) {
   domainInfo.queryOne.forEach((item) => {
+    console.log('Removing:', item)
     const el = document.body.querySelector(item)
     if (el) {
       el.remove()
@@ -89,19 +108,55 @@ function removeByQuery (domainInfo) {
   })
 }
 
-if (matchedDomainKey) {
-  const domainInfo = domainData[matchedDomainKey]
+function checkIfWordpress () {
+  const metaTag = document.querySelector('meta[property="og:image"]')
+  if (metaTag === null) {
+    return false
+  }
 
-  // Check and perform actions based on the domain data
+  if (metaTag.content.includes('wp-content')) {
+    return true
+  }
+}
+
+function cleanSite (domainInfo) {
+  GM_notification('This domain should be checked in the global script', "Gravityfargo's Global Script")
+
   if (domainInfo.ids.length > 0) {
+    console.log('Removing by ID')
     removeById(domainInfo)
   }
 
   if (domainInfo.queryAll.length > 0) {
+    console.log('Removing by query all')
     removeByQueryAll(domainInfo)
   }
 
   if (domainInfo.queryOne.length > 0) {
+    console.log('Removing by query one')
     removeByQuery(domainInfo)
   }
 }
+
+function init () {
+  if (matchedDomainKey) {
+    console.log('Domain matched:', windowLocationHost)
+
+    const domainInfo = domainData[matchedDomainKey]
+    cleanSite(domainInfo)
+    return
+  }
+
+  if (checkIfWordpress()) {
+    console.log('This is a Wordpress site')
+    cleanSite(wordpressData)
+  }
+}
+
+// document-end is too early for some sites
+// the benefit of the observer is that it runs before the page is fully loaded
+// which would be the case with document-idle
+
+window.addEventListener('load', function () {
+  init()
+})
