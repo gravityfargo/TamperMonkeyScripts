ad54530b385177b4458ede295cea825b0090fd16
Nathan Price <gravityfargo@gmail.com>
2024-10-24 17:49:00 -0400

    bump

--- GLOBAL/functions.js
+++ GLOBAL/functions.js
@@ -9,7 +9,6 @@
  *
  * @param {string} selector - The CSS selector for the element to wait for.
  * @param {function} callback - The function to execute once the element is found or when the timeout occurs.
- *                              Receives the found element as an argument, or `null` if the timeout is reached.
  * @param {number} [timeout=10000] - The time in milliseconds to wait before timing out. Default is 10 seconds.
  *
  * @returns {void} - This function does not return a value.
@@ -44,33 +43,30 @@ function waitForElementToAppear (selector, callback, timeout = 10000) {
 }
 
 /**
- * Continuously monitors a specific DOM element for changes using MutationObserver.
- * The function invokes the callback whenever a change occurs in the element or its subtree.
- * If the element is not found when the function is called, it immediately returns without waiting.
- * The observer automatically disconnects after the specified timeout.
+ * Continuously monitors for new/deleted children of a specific DOM element using MutationObserver
+ * and stops observing after a specified timeout.
  *
  * @param {string} selector - The CSS selector for the element to monitor.
  * @param {function} callback - The function to execute whenever a change occurs in the element or its subtree.
- *                              Receives four arguments: the mutation records, the observed element,
- *                              a boolean `isTimeout` to indicate if the callback was triggered by timeout,
- *                              and a boolean `isElementNotFound` to signal if the element was not found initially.
  * @param {number} [timeout=10000] - The time in milliseconds to monitor before timing out (default is 10 seconds).
  *
  * @returns {void} - This function does not return a value if the element is not found.
  *
  * @example
- * monitorElementForChangesWithTimeout('body', (mutations, element, isTimeout, isElementNotFound) => {
+ * // Monitor the body element for changes for a maximum of 5 seconds
+ * monitorElementsChildren('body', (mutations, element, isTimeout, isElementNotFound) => {
  *  if (isTimeout) {
  *    console.log('Monitoring stopped due to timeout.');
  *  } else if (isElementNotFound) {
  *   console.log('Element not found. Monitoring stopped.');
  *  } else {
- *    console.log('Changes detected in body:', mutations);
+ *    console.log(`Changes detected in ${element}`);
+ *    console.log(`Those changes are: ${mutations}`);
  *  }
  * }, 5000);
  *
  */
-function monitorElementForChangesWithTimeout (selector, callback, timeout = 10000) {
+function monitorElementsChildrenWithTimeout (selector, callback, timeout = 10000) {
   const element = document.querySelector(selector)
 
   if (!element) {
@@ -85,9 +81,7 @@ function monitorElementForChangesWithTimeout (selector, callback, timeout = 1000
 
   observer.observe(element, {
     childList: true,
-    subtree: true,
-    attributes: true,
-    characterData: true
+    subtree: true
   })
 
   // Set a timeout to stop observing
@@ -98,32 +92,37 @@ function monitorElementForChangesWithTimeout (selector, callback, timeout = 1000
 }
 
 /**
- * Continuously monitors a specific DOM element for changes using MutationObserver.
- * The function invokes the callback whenever a change occurs in the element or its subtree.
- * If the element is not found when the function is called, it immediately returns without waiting.
- * The observer will not stop until manually disconnected.
+ * Continuously monitors for new/deleted children of a specific DOM element using MutationObserver.
  *
  * @param {string} selector - The CSS selector for the element to monitor.
  * @param {function} callback - The function to execute whenever a change occurs in the element or its subtree.
- *                              Receives three arguments: the mutation records, the observed element, and a boolean
- *                              to indicate if the element was not found initially.
  *
- * @returns {function} - Returns a function that can be called to manually stop the observer.
+ * @returns {function} - Function to manually stop the observer.
  *
  * @example
- * const stopMonitoring = monitorElementForChanges('body', (mutations, element, isElementNotFound) => {
+ * // Stoppable monitoring
+ * const stopMonitoring = monitorElementsChildren('body', (mutations, element, isElementNotFound) => {
  *  if (isElementNotFound) {
  *    console.log('Element not found. Monitoring stopped.');
  *  } else {
- *    console.log('Changes detected in body:', mutations);
+ *    console.log(`Changes detected in ${element}`);
+ *    console.log(`Those changes are: ${mutations}`);
  *  }
  * });
+ * stopMonitoring(); // Manually stop monitoring later
  *
- * // Manually stop monitoring later
- * stopMonitoring();
- *
+ * @example
+ * // continuous monitoring
+ * monitorElementsChildren('nav', (mutations, element, isElementNotFound) => {
+ *  if (isElementNotFound) {
+ *    console.log('Element not found. Monitoring stopped.');
+ *  } else {
+ *    console.log(`Changes detected in ${element}`);
+ *    console.log(`Those changes are: ${mutations}`);
+ *  }
+ * });
  */
-function monitorElementForChanges (selector, callback) {
+function monitorElementsChildren (selector, callback) {
   const element = document.querySelector(selector)
 
   if (!element) {
@@ -183,12 +182,46 @@ function isNodelist (obj) {
 }
 
 /**
- * Finds all elements that match the given CSS selector within a specified parent element (or the entire document),
+ * Checks if an object is null.
+ *
+ * Not really necessary, but I think it makes code more readable.
+ *
+ * @param {*} obj Source object to check
+ *
+ * @returns {boolean} Returns true if the object is null
+ *
+ * @example
+ * const elements = document.querySelectorAll('div');
+ * console.log(isNull(elements)); // false
+ * console.log(isNull(null)); // true
+ */
+function isNull (obj) {
+  if (obj === null) {
+    return true
+  }
+  return false
+}
+
+/**
+ * Just logs to the console. Save some keystrokes.
+ *
+ * @param {string} message - The message to log to the console.
+ *
+ * @returns {void}
+ *
+ * @example
+ * glog('Hello, World!');
+ */
+function glog (message) {
+  console.log(message)
+}
+
+/**
+ * Finds all elements with the given CSS selector within a specified parent element
  * and removes each element from the DOM.
  *
  * @param {string} selector - The CSS selector used to identify elements to be removed.
  * @param {ParentNode} [parent=document] - The parent node within which to search for elements. Defaults to the document.
- * @param {boolean} [log=false] - Whether to log messages to the console. Defaults to false.
  *
  * @returns {number} - The number of elements removed.
  *
@@ -200,32 +233,30 @@ function isNodelist (obj) {
  * const parent = document.getElementById('container');
  * querySelectorAllDelete('.to-remove', parent);
  *
- * // Deletes and logs each element being removed
- * querySelectorAllDelete('.to-remove', document, true);
- *
  */
-function querySelectorAllDelete (selector, parent = document, log = false) {
-  if (log) { console.log('querySelectorAllDelete', selector) }
-  let removedCount = 0
+function querySelectorAllDelete (selector, parent = document) {
+  const removedElements = []
 
   const elements = parent.querySelectorAll(selector)
+  if (!isNodelist(elements) && isNull(elements)) { return }
+
   elements.forEach((el) => {
-    if (log) console.log(el)
+    if (!isElement(el)) { return }
     el.remove()
-    removedCount++
+    removedElements.push(el)
   })
 
-  return removedCount
+  const logStr = `querySelectorAllDelete() \n\t Removed ${removedElements.length} elements.\n`
+  if (removedElements.length > 0) { console.log(logStr, removedElements) }
 }
 
 /**
- * Removes elements from a NodeList or single element if they have the specified attribute.
+ * Removes element(s) from a NodeList if the specified attribute is present.
  *
- * @param {NodeList|Element} elements - The elements to check.
+ * @param {NodeList|Element} elements - The element(s) to check.
  * @param {string} attribute - The attribute to check for.
- * @param {boolean} [log=false] - Whether to log messages to the console. Defaults to false.
  *
- * @returns {number} - The number of elements removed.
+ * @returns {void}
  * @example
  * // Removes all elements with the 'data-remove' attribute in a NodeList
  * const elements = document.querySelectorAll('div');
@@ -235,69 +266,87 @@ function querySelectorAllDelete (selector, parent = document, log = false) {
  * const element = document.querySelector('div');
  * removeIfHasAttribute(element, 'data-remove');
  *
- * // Removes elements with logging enabled
- * removeIfHasAttribute(elements, 'data-remove', true);
  */
-function removeIfHasAttribute (elements, attribute, log = false) {
+function removeIfHasAttribute (elements, attribute) {
+  // console.log(elements, attribute)
+  if (!isNodelist(elements) && isNull(elements)) { return }
   if (!isNodelist(elements)) { elements = [elements] }
-  if (log) { console.log('removeIfHasAttribute', attribute) }
 
-  let removedCount = 0
+  const removedElements = []
 
   elements.forEach((el) => {
     if (!isElement(el)) { return }
-    if (log) { console.log(el) }
 
     if (el.hasAttribute(attribute)) {
       el.remove()
-      removedCount++
-      if (log) { console.log('Removed!') }
+      removedElements.push(el)
     }
   })
 
-  return removedCount
+  const logStr = `removeIfHasAttribute() \t Removed ${removedElements.length} elements.`
+  if (removedElements.length > 0) { console.log(logStr, removedElements) }
 }
 
 /**
- * Removes elements if their specified attribute includes any of the provided stubs.
+ * Removes element(s) if the specified attribute contains a keyword from the provided stub(s).
  *
- * @param {NodeList|Element} elements - The elements to check.
+ * @param {NodeList|Element} elements - The element(s) to check.
  * @param {string} attribute - The attribute to check.
- * @param {string|string[]} stubs - The value(s) to match within the attribute value.
- * @param {boolean} [log=false] - Whether to log messages to the console. Defaults to false.
+ * @param {string|string[]} stubs - The value(s) to match.
  *
- * @returns {number} - The number of elements removed.
+ * @returns {void}
  *
  * @example
  * // Removes elements where the 'class' attribute includes 'highlight' or 'selected'
  * const elements = document.querySelectorAll('div');
- * removeIfAttributeIncludes(elements, 'class', ['highlight', 'selected']);
+ * const stubs = ['highlight', 'selected'];
+ * removeIfAttributeIncludes(elements, 'class', stubs);
  *
  * // Removes elements with a single attribute value stub
  * removeIfAttributeIncludes(elements, 'class', 'active');
  *
- * // Removes and logs elements being removed
- * removeIfAttributeIncludes(elements, 'class', 'active', true);
  */
-function removeIfAttributeIncludes (elements, attribute, stubs, log = false) {
+function removeIfAttributeIncludes (elements, attribute, stubs) {
+  // console.log(elements, attribute, stubs)
+  if (!isNodelist(elements) && isNull(elements)) { return }
   if (!isNodelist(elements)) { elements = [elements] }
   if (!Array.isArray(stubs)) { stubs = [stubs] }
 
-  if (log) { console.log('removeIfAttributeIncludes', attribute, stubs) }
-
-  let removedCount = 0
+  const removedElements = []
 
   elements.forEach((el) => {
     if (!isElement(el)) { return }
-    if (log) { console.log(el) }
 
     const attrValue = el.getAttribute(attribute)
+    console.log(el)
     if (attrValue && stubs.some(stub => attrValue.includes(stub))) {
       el.remove()
-      removedCount++
-      if (log) { console.log('Removed!') }
+      removedElements.push(el)
     }
   })
 
-  return removedCount
+  const logStr = `removeIfAttributeIncludes() \t Removed ${removedElements.length} elements.`
+  if (removedElements.length > 0) { console.log(logStr, removedElements) }
+}
+
+/**
+ * Removes element(s) from a NodeList if the specified attribute is present.
+ *
+ * @param {string} selector - The CSS selector used to find the element to check.
+ * @param {ParentNode} [parent=document] - The parent node within which to search for elements. Defaults to the document.
+ * @returns  {void}
+ *
+ * @example
+ * // Removes the first <h1>> found in document.body
+ * querySelectorDelete('h1', document.body);
+ *
+ * // Removes the first <h3> found in the document
+ * querySelectorDelete('h3');
+ */
+function querySelectorDelete (selector, parent = document) {
+  const element = parent.querySelector(selector)
+  if (!isElement(element)) { return }
+  element.remove()
+
+  console.log(`querySelectorDelete() \t Removed ${element}`)
 }
